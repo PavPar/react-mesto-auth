28de6fc5076462ac5740bbf1af087020a2913cca
Asha <paramonpav@yandex.ru>
2020-09-30 23:16:11 +0300

    feat: Add delete button

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { CurrentUserContext } from '../../context/CurrentUserContext';
+
 
 export default function Card({ card, onCardClick }) {
+    const currentUser = React.useContext(CurrentUserContext);
+
     const name = card['name'];
     const link = card['link'];
     const likes = card['likes'].length;
+
+    // Определяем, являемся ли мы владельцем текущей карточки
+    const isOwn = card.owner._id === currentUser._id;
+
+    // Создаём переменную, которую после зададим в `className` для кнопки удаления
+    const cardDeleteButtonClassName = (
+        `card__button card__button_type-delete ${!isOwn && 'card__button_state-invisible'}`
+    );
 
     function handleClick() {
         onCardClick(card);
@@ -15,7 +27,7 @@ export default function Card({ card, onCardClick }) {
             <h2 className="card__title">{name}</h2>
             <button className="card__button card__button_type-like"></button>
             <p className="card__like-counter">{likes}</p>
-            <button className="card__button card__button_type-delete"></button>
+            <button className={cardDeleteButtonClassName}></button>
         </div>
     )
 } 
