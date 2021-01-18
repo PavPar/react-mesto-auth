1f35d90ba06a21bf1b7eba2fca875edd7e45e5d7
Asha <paramonpav@yandex.ru>
2021-01-18 15:25:43 +0300

    feat/fix: adaptive interface + component fixing

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "./Header";
 import headerLogo from "../images/logo.svg";
+import errLogo from "../images/err.svg";
+import sucLogo from "../images/suc.svg";
+
 import Form from './Form';
 import ValidationField from './ValidationField';
 import Popup from './Popup';
@@ -21,43 +24,45 @@ export default function Register() {
             <Header src={headerLogo} >
                 <p className="header__link">Регистрация</p>
             </Header>
+            <section className="auth">
+                <Form
+                    name="card"
 
-            <Form
-                name="card"
+                    title="Новое место"
+                    btnText="Добавить"
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                        setIsImagePopupOpen(true);
+                    }}
 
-                title="Новое место"
-                btnText="Добавить"
-                onSubmit={(event) => {
-                    event.preventDefault();
-                    setIsImagePopupOpen(true);
-                }}
+                >
+                    <ValidationField
+                        id="form__input-card-title"
+                        type="text"
+                        placeholder="Название"
+                        name="title"
+                        minLength="2"
+                        maxLength="30"
+                        required={true}
+                    />
+                    <ValidationField
+                        required={true}
+                        id="form__input-card-link"
+                        type="url"
+                        placeholder="Ссылка на изображение"
+                        name="src"
+                    />
 
-            >
-                <ValidationField
-                    id="form__input-card-title"
-                    type="text"
-                    placeholder="Название"
-                    name="title"
-                    minLength="2"
-                    maxLength="30"
-                    required={true}
-                />
-                <ValidationField
-                    required={true}
-                    id="form__input-card-link"
-                    type="url"
-                    placeholder="Ссылка на изображение"
-                    name="src"
-                />
-
-            </Form>
+                </Form>
+                <p className="auth__text">Уже зарегистрированы? Войти</p>
+            </section>
 
             <Popup
                 onClose={closeAllPopups}
                 isOpen={isImagePopupOpen}
                 name={'message'}
             >
-                <img className="popup__image" src={headerLogo} alt="fuck"></img>
+                <img className="popup__image" src={sucLogo} alt="fuck"></img>
                 <h2 className="popup__title">{"Все путем!"}</h2>
             </Popup>
         </>
